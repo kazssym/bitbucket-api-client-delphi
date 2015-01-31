206135ff4be212bfca0483c384c072d353526374
Kaz Nishimura <kazssym@vx68k.org>
2015-01-31 21:27:34 +0900

    Created new package projects for Delphi.

---
+++ bitbucketapi/BitbucketAPI.Types.pas
@@ -0,0 +1,64 @@
+{
+  BitbucketAPI.Types - Bitbucket API common types
+  Copyright (C) 2014-2015 Kaz Nishimura
+
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit BitbucketAPI.Types;
+
+interface
+
+uses System.Classes, System.SysUtils;
+
+type
+  {
+    Base exception for this package.
+  }
+  EBitbucketAPIException = class(Exception)
+  end;
+
+  IBitbucketAPI = interface
+    ['{00EB20C9-B842-4820-969F-4D5FFAD34DEF}']
+  end;
+
+  IBitbucketSession = interface
+    ['{48AE1846-0EA8-4DA3-9234-B3FA3190D280}']
+  end;
+
+  {
+    Client credentials.
+  }
+  TBitbucketClientCredentials = class(TPersistent)
+  private
+  var
+    FID: string;
+    FSecret : string;
+  published
+    property ID : string read FID write FID;
+    property Secret : string read FSecret write FSecret;
+  end;
+
+  {
+    Factory object for Bitbucket sessions.
+  }
+  IBitbucketSessionFactory = interface
+    ['{AAB34981-CD69-45AC-8D19-32FE4217480D}']
+    function GetSession(ClientCredentials: TBitbucketClientCredentials)
+        : IBitbucketSession; stdcall;
+  end;
+
+implementation
+
+end.
---
+++ bitbucketapi/BitbucketAPI.pas
@@ -0,0 +1,77 @@
+{
+  BitbucketAPI - Bitbucket API Client
+  Copyright (C) 2014-2015 Kaz Nishimura
+
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit BitbucketAPI;
+
+interface
+
+uses System.Classes, BitbucketAPI.Types;
+
+type
+  TBitbucketAPI = class(TComponent)
+  private
+  class var
+    FSessionFactory: IBitbucketSessionFactory;
+  var
+    FClientCredentials: TBitbucketClientCredentials;
+  public
+    class procedure SetSessionFactory(SessionFactory
+        : IBitbucketSessionFactory); static;
+    constructor Create(Owner: TComponent); override;
+    destructor Destroy; override;
+    function GetClientCredentials: TBitbucketClientCredentials;
+    function GetSession: IBitbucketSession;
+  published
+    property ClientCredentials: TBitbucketClientCredentials
+        read FClientCredentials;
+  end;
+
+implementation
+
+class procedure TBitbucketAPI.SetSessionFactory(SessionFactory
+    : IBitbucketSessionFactory);
+begin
+  FSessionFactory := SessionFactory;
+end;
+
+constructor TBitbucketAPI.Create(Owner: TComponent);
+begin
+  inherited Create(Owner);
+  FClientCredentials := TBitbucketClientCredentials.Create;
+end;
+
+destructor TBitbucketAPI.Destroy;
+begin
+  FClientCredentials.Free;
+  inherited Destroy;
+end;
+
+function TBitbucketAPI.GetClientCredentials;
+begin
+  Result := FClientCredentials;
+end;
+
+function TBitbucketAPI.GetSession;
+begin
+  if FSessionFactory <> nil then
+  begin
+    Result := FSessionFactory.GetSession(FClientCredentials);
+  end;
+end;
+
+end.
